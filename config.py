82ca9a57ba6d164abc99a9b582d36909dbdd998c
Bernard Thimotius Turnip <109124722+Bernardtrnp@users.noreply.github.com>
2025-12-11 06:52:24 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+# =================================================================
+#                   KONFIGURASI CHRONOREPLY BOT
+# =================================================================
+
+# GANTI DENGAN USER TOKEN DISCORD ANDA (Wajib)
+# WARNING: JANGAN BERIKAN TOKEN INI KEPADA SIAPA PUN.
+USER_TOKEN = ""
+
+# PREFIX COMMAND
+PREFIX = "/"
+
+# TIMEZONE DEFAULT BOT
+# Gunakan format standar IANA. Contoh: 'Asia/Jakarta', 'Europe/London'
+DEFAULT_TZ = 'Asia/Jakarta' 
+
+# FILE PENYIMPANAN DATA JADWAL
+TASK_FILE = 'tasks.json'
